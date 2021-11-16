5c31076ef6a44ef786288f22ade0dfe7d7830040
Phil Davies <github@phildavies.co.uk>
2021-11-16 12:08:58 +0000

    add GdsListContentRenderer for ol & ul elements

--- src/SFA.DAS.Employer.FrontDoor.Contentful.TestHarness/Program.cs
+++ src/SFA.DAS.Employer.FrontDoor.Contentful.TestHarness/Program.cs
@@ -18,6 +18,7 @@ namespace SFA.DAS.Employer.FrontDoor.Contentful.TestHarness
                 "",
                 "0liyzri8haz6");
 
+            //todo: get all
             var entry = await client.GetEntry<Scheme>("6YMOVJcUS66vdhyP4q9CAs");
 
             var htmlRenderer = new HtmlRenderer();
