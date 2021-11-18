022cb1c6a882aee80193d27063da309f2ca6bfa2
Phil Davies <github@phildavies.co.uk>
2021-11-18 11:37:25 +0000

    add analyzers to web project

--- src/SFA.DAS.Employer.FrontDoor.Web/Program.cs
+++ src/SFA.DAS.Employer.FrontDoor.Web/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace SFA.DAS.Employer.FrontDoor.Web
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
