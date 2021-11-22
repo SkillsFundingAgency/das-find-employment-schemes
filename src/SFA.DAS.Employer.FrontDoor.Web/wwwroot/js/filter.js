39e9dda73ac7c72c3474ae59f937c96353002a14
Phil Davies <github@phildavies.co.uk>
2021-11-22 12:48:48 +0000

    filter js wip

--- src/SFA.DAS.Employer.FrontDoor.Web/wwwroot/js/filter.js
+++ src/SFA.DAS.Employer.FrontDoor.Web/wwwroot/js/filter.js
@@ -5,6 +5,10 @@
 
 /*todo: we'll check for IE and let them use the non-javascript enhanced version*/
 
+/*todo:use modules, remove globals! ts?*/
+
+var fetchResults = true;
+
 // http://stackoverflow.com/questions/4197591/parsing-url-hash-fragment-identifier-with-javascript
 //function getHashParams() {
 
@@ -25,4 +29,41 @@ function getHashParams() {
     const hashParams = new URLSearchParams(window.location.hash.substr(1)); // skip the first char (#)
 
     return hashParams;
+}
+
+function updateFragmentFromCheckboxes() {
+    // checkboxes
+    var newFilter = [];
+    var newFilterStr = '';
+    $('.filter-schemes :checkbox').each(function () {
+        const $this = $(this);
+        var filterId;
+        if ($this.prop('checked')) {
+            filterId = $this.data('filterid');
+            if ($.inArray(filterId, newFilter) === -1) {
+                newFilter.push(filterId);
+            }
+        }
+    });
+
+    if (newFilter.length > 0) {
+        newFilterStr = newFilter.join(",");
+    }
+
+    const hashParams = getHashParams();
+    hashParams['filter'] = newFilterStr;
+    setHashParams(hashParams, true);
+}
+
+var fetchResults = true;
+function setHashParams(hashParams, updateResults) {
+    var fragment = "";
+    $.each(hashParams, function (key, value) {
+        fragment += key + '=' + value + '&';
+    });
+
+    //todo: only need to update if changed
+    fetchResults = updateResults;
+    //todo: check fragment.length === 0
+    window.location.hash = fragment.substr(0, fragment.length - 1);
 }
