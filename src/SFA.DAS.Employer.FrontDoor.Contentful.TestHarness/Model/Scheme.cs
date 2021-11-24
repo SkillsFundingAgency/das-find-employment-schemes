52c8c2cd7c11b292e4f3552bf8aca6d2bb52d85f
Phil Davies <github@phildavies.co.uk>
2021-11-24 09:20:09 +0000

    utility to generate content as code wip

--- src/SFA.DAS.Employer.FrontDoor.Contentful.TestHarness/Model/Scheme.cs
+++ src/SFA.DAS.Employer.FrontDoor.Contentful.TestHarness/Model/Scheme.cs
@@ -5,8 +5,20 @@ namespace SFA.DAS.Employer.FrontDoor.Contentful.TestHarness.Model
     public class Scheme
     {
         public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string ShortCost { get; set; }
+        public string ShortBenefits { get; set; }
+        public string ShortTime { get; set; }
         public string Url { get; set; }
-        public Document DetailsPageOverride { get; set; }
-        public Document Description { get; set; }
+        public Document? DetailsPageOverride { get; set; }
+        public Document? Description { get; set; }
+        public Document? Cost { get; set; }
+        public Document? Responsibility { get; set; }
+        public Document? Benefits { get; set; }
+        public Document? CaseStudies { get; set; }
+        public string? OfferHeader { get; set; }
+        public Document? Offer { get; set; }
+        //todo: add size to contentful
+        //public int Size { get; set; }
     }
 }
--- src/SFA.DAS.Employer.FrontDoor.Contentful.TestHarness/Program.cs
+++ src/SFA.DAS.Employer.FrontDoor.Contentful.TestHarness/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Contentful.Core;
 using Contentful.Core.Models;
@@ -19,14 +22,41 @@ namespace SFA.DAS.Employer.FrontDoor.Contentful.TestHarness
                 "0liyzri8haz6");
 
             //todo: get all
-            var entry = await client.GetEntry<Scheme>("6YMOVJcUS66vdhyP4q9CAs");
-            //var entries = await client.GetEntries<>();
+            //var entry = await client.GetEntry<Scheme>("6YMOVJcUS66vdhyP4q9CAs");
+            var entries = await client.GetEntries<Scheme>();
 
             var htmlRenderer = new HtmlRenderer();
+            htmlRenderer.AddRenderer(new GdsCtaContentRenderer(htmlRenderer.Renderers));
             htmlRenderer.AddRenderer(new GdsHeadingRenderer(htmlRenderer.Renderers));
+            htmlRenderer.AddRenderer(new GdsHorizontalRulerContentRenderer());
             htmlRenderer.AddRenderer(new GdsHyperlinkContentRenderer(htmlRenderer.Renderers));
-            string description = await htmlRenderer.ToHtml(entry.Description);
-            string detailsPageOverride = await htmlRenderer.ToHtml(entry.DetailsPageOverride);
+            htmlRenderer.AddRenderer(new GdsListContentRenderer(htmlRenderer.Renderers));
+
+            var content = new StringBuilder();
+            foreach (Scheme entry in entries)
+            {
+                content.Append($"new Scheme(\"{entry.Name}\",");
+                content.Append($"\"{entry.ShortDescription}\",");
+                content.Append($"\"{entry.ShortCost}\",");
+                content.Append($"\"{entry.ShortBenefits}\",");
+                content.Append($"\"{entry.ShortTime}\",");
+                content.Append($"\"{entry.Url}\", 0, \"{await AsHtmlString(entry.DetailsPageOverride, htmlRenderer)}\"");
+                content.Append($"\"{await AsHtmlString(entry.Description, htmlRenderer)}\",");
+                content.Append($"\"{await AsHtmlString(entry.Cost, htmlRenderer)}\",");
+                content.Append($"\"{await AsHtmlString(entry.Responsibility, htmlRenderer)}\",");
+                content.Append($"\"{await AsHtmlString(entry.Benefits, htmlRenderer)}\",");
+                content.Append($"\"{await AsHtmlString(entry.CaseStudies, htmlRenderer)}\",");
+                content.Append($"\"{entry.OfferHeader}\",");
+                content.Append($"\"{await AsHtmlString(entry.Offer, htmlRenderer)}\",");
+            }
+        }
+
+        private static async Task<string> AsHtmlString(Document? document, HtmlRenderer htmlRenderer)
+        {
+            if (document == null)
+                return "new HtmlString(\"\")";
+
+            return $"new HtmlString(@\"{(await htmlRenderer.ToHtml(document)).Replace("\"", "\"\"")}\")";
         }
     }
 }
