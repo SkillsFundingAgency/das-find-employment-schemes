334949d2495d5c63df8cc97369608b27c239b22a
Phil Davies <github@phildavies.co.uk>
2021-12-17 13:47:20 +0000

    on mobile, when js is enabled, when the user taps filter schemes or the close button, scroll page to top of scheme results (including the filter schemes button)

--- src/SFA.DAS.FindEmploymentSchemes.Web/Controllers/SchemesController.cs
+++ src/SFA.DAS.FindEmploymentSchemes.Web/Controllers/SchemesController.cs
@@ -6,9 +6,7 @@ using SFA.DAS.FindEmploymentSchemes.Web.ViewModels;
 
 namespace SFA.DAS.FindEmploymentSchemes.Web.Controllers
 {
-    //todo: in mobile when js enabled, want filter schemes button
     //todo: remove number of schemes count when non-javascript??
-    //todo: on mobile, when js is enabled, when user clicks filter schemes, or close, ensure at top of page (or scheme results)
 
     public class SchemesController : Controller
     {
