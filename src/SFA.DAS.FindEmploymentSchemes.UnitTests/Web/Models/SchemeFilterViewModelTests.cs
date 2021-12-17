67b81f101544e5bce90b4b3b433ed34964c2b4d9
Ian Rose <ian.rose@digital.education.gov.uk>
2021-12-17 16:38:41 +0000

    Model and ViewModel tests

---
+++ src/SFA.DAS.FindEmploymentSchemes.UnitTests/Web/Models/SchemeFilterViewModelTests.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using SFA.DAS.FindEmploymentSchemes.Web.ViewModels;
+
+
+namespace SFA.DAS.FindEmploymentSchemes.UnitTests.Web.ViewModels
+{
+    public class SchemeFilterViewModelTests
+    {
+        [Theory]
+        [ClassData(typeof(SchemeFilterViewModelTestData))]
+
+        public void Constructor_AllFiltersCount(int expectedAllFiltersCount, SchemeFilterViewModel model)
+        {
+            Assert.Equal(expectedAllFiltersCount, model.allFilters.Count());
+        }
+
+        public class SchemeFilterViewModelTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] {  0, new SchemeFilterViewModel(new string[] { }, new string[] { }, new string[] { }) };
+                yield return new object[] {  4, new SchemeFilterViewModel(new string[] { "abc", "def" }, new string[] { "ghi" }, new string[] { "xyz" }) };
+                yield return new object[] { 10, new SchemeFilterViewModel(new string[] { "abc", "def", "ghi" }, new string[] { "jk", "l", "mn" }, new string[] { "o", "pqrs", "tuvw", "xyz" }) };
+            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
---
+++ src/SFA.DAS.FindEmploymentSchemes.UnitTests/Web/Services/FilterServiceTests.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using SFA.DAS.FindEmploymentSchemes.Web;
+using SFA.DAS.FindEmploymentSchemes.Web.Content;
+using SFA.DAS.FindEmploymentSchemes.Web.Models;
+using SFA.DAS.FindEmploymentSchemes.Web.Services;
+using SFA.DAS.FindEmploymentSchemes.Web.ViewModels;
+
+
+namespace SFA.DAS.FindEmploymentSchemes.UnitTests.Web.Services
+{
+    public class FilterServiceTests
+    {
+        private readonly IServiceProvider _services = Program.CreateHostBuilder(new string[] { }).Build().Services;
+        private IFilterService _service;
+
+        [Theory]
+        [ClassData(typeof(FilterServiceTestData))]
+        public void ApplyFilters_Result(IEnumerable<Scheme> expectedSchemes, SchemeFilterViewModel filters)
+        {
+            _service = _services.GetRequiredService<IFilterService>();
+            
+            HashSet<Scheme> expected = new HashSet<Scheme>(expectedSchemes);
+            HashSet<Scheme> applied = new HashSet<Scheme>(_service.ApplyFilter(filters).Schemes);
+            Assert.True(expected.SetEquals(applied));
+        }
+
+        public class FilterServiceTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { new Scheme[] { }, new SchemeFilterViewModel(new string[] { }, new string[] { }, new string[] { } ) };
+                yield return new object[] { new Scheme[] { }, new SchemeFilterViewModel(new string[] { }, new string[] { }, new string[] { }) };
+                yield return new object[] { new Scheme[] { }, new SchemeFilterViewModel(new string[] { }, new string[] { }, new string[] { }) };
+            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
