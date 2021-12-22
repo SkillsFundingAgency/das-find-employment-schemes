44eeec998583aafe88fe9b735c86f8a40ae09ca2
Phil Davies <github@phildavies.co.uk>
2021-12-22 10:17:47 +0000

    move homepage preamble into cms

--- src/SFA.DAS.FindEmploymentSchemes.Web/Models/HomeModel.cs
+++ src/SFA.DAS.FindEmploymentSchemes.Web/Models/HomeModel.cs
@@ -1,15 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Html;
+using System.Collections.Generic;
 
 namespace SFA.DAS.FindEmploymentSchemes.Web.Models
 {
     public class HomeModel
     {
+        public IHtmlContent Preamble { get; }
         public IEnumerable<Scheme> Schemes { get; }
         public IEnumerable<FilterGroupModel> FilterGroupModels { get; }
         public bool EnsureSchemesAreVisible { get; }
 
-        public HomeModel(IEnumerable<Scheme> schemes, IEnumerable<FilterGroupModel> filterGroupModels, bool ensureSchemesAreVisible = false)
+        public HomeModel(IHtmlContent preamble, IEnumerable<Scheme> schemes, IEnumerable<FilterGroupModel> filterGroupModels, bool ensureSchemesAreVisible = false)
         {
+            Preamble = preamble;
             Schemes = schemes;
             FilterGroupModels = filterGroupModels;
             EnsureSchemesAreVisible = ensureSchemesAreVisible;
