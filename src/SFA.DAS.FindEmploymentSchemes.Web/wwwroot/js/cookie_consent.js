7284f8808938ac45c2f33a5ae65336d728b65d79
Ian Rose <ian.rose@digital.education.gov.uk>
2022-01-10 17:35:59 +0000

    Single analytics preference cookie

--- src/SFA.DAS.FindEmploymentSchemes.Web/wwwroot/js/cookie_consent.js
+++ src/SFA.DAS.FindEmploymentSchemes.Web/wwwroot/js/cookie_consent.js
@@ -1,14 +1,14 @@
 ﻿
 $(document).ready(function () {
-    if (window.GOVUK.cookie("DASAcceptCookies") || window.GOVUK.cookie("DASRejectCookies")) {
+    if (window.GOVUK.cookie("DASAcceptCookies") == "true" || window.GOVUK.cookie("DASAcceptCookies") == "false") {
         $("div.govuk-cookie-banner").hide();
     } else {
         $("button.cookie-consent-button").click(function () {
             if ($(this).hasClass("cookies-accept")) {
-                window.GOVUK.cookie("DASAcceptCookies", true, { days: 365 });
+                window.GOVUK.cookie("DASAcceptCookies", "true", { days: 365 });
                 $("div#cookie-accept-message").show();
             } else {
-                window.GOVUK.cookie("DASRejectCookies", true, { days: 365 });
+                window.GOVUK.cookie("DASAcceptCookies", "false", { days: 365 });
                 $("div#cookie-reject-message").show();
             }
 
