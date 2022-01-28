077650f96a026b2c5cf8b4c9595ee816752dd065
Phil Davies <github@phildavies.co.uk>
2022-01-28 14:23:42 +0000

    unit tests wip

--- src/SFA.DAS.FindEmploymentSchemes.UnitTests/Contentful/Services/ContentServiceTests.cs
+++ src/SFA.DAS.FindEmploymentSchemes.UnitTests/Contentful/Services/ContentServiceTests.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using Contentful.Core.Models;
+using AutoFixture;
 using Contentful.Core;
+using Contentful.Core.Models;
+using Contentful.Core.Search;
 using FakeItEasy;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.FindEmploymentSchemes.Contentful.Model.Api;
 using SFA.DAS.FindEmploymentSchemes.Contentful.Services;
 using Xunit;
-using AutoFixture.Kernel;
-using AutoFixture;
-using Contentful.Core.Search;
-using Microsoft.AspNetCore.Html;
-using System.Threading;
-using SFA.DAS.FindEmploymentSchemes.Contentful.Model.Api;
 
-namespace SFA.DAS.FindEmploymentSchemes.UnitTests.Web.Controllers
+namespace SFA.DAS.FindEmploymentSchemes.UnitTests.Contentful.Services
 {
     public class ContentServiceTests
     {
         [Theory]
         [InlineData("pay--the-name", "the name")]
+        [InlineData("pay--thename", "thename")]
+        [InlineData("pay--the-name", "the-name")]
+        //todo: stop double spaces, so code doesn't get confused with prefix/name separator?
+        [InlineData("pay--the--name", "the  name")]
+        [InlineData("pay--", "")]
+        [InlineData("pay--1234567890-qwertyuiop-asdfghjkl-zxcvbnm", "1234567890 qwertyuiop asdfghjkl zxcvbnm")]
         public async Task ToContentScheme_FilterIdTests(string expectedFilterAspectId, string filterName)
         {
             var contentfulClient = A.Fake<IContentfulClient>();
@@ -27,10 +31,6 @@ namespace SFA.DAS.FindEmploymentSchemes.UnitTests.Web.Controllers
             var logger = A.Fake<ILogger<ContentService>>();
 
             var fixture = new Fixture();
-
-            //var schemes = fixture.CreateMany<Scheme>(1).ToArray();
-            //schemes[0].PayFilterAspects = fixture.CreateMany<Filter>(1).ToList();
-            //schemes[0].PayFilterAspects.First().Name = filterName;
 
             var pagesCollection = new ContentfulCollection<Page> {Items = Array.Empty<Page>()};
             A.CallTo(() => contentfulClient.GetEntries(A<QueryBuilder<Page>>.Ignored, A<CancellationToken>.Ignored))
