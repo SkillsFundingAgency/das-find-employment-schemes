ad9555c87addc4d6fe81491bf68c51faa3813c8c
Phil Davies <github@phildavies.co.uk>
2022-02-03 09:05:00 +0000

    wip

--- src/SFA.DAS.FindEmploymentSchemes.Contentful/Extensions/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.FindEmploymentSchemes.Contentful/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,10 @@
-﻿using Contentful.AspNetCore;
+﻿using System.Net.Http;
+using Contentful.AspNetCore;
+using Contentful.Core;
+using Contentful.Core.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using SFA.DAS.FindEmploymentSchemes.Contentful.Services;
 
 namespace SFA.DAS.FindEmploymentSchemes.Contentful.Extensions
@@ -15,6 +19,17 @@ namespace SFA.DAS.FindEmploymentSchemes.Contentful.Extensions
                 .AddContentful(configuration)
                 .AddTransient(sp => ContentService.CreateHtmlRenderer())
                 .AddSingleton<IContentService, ContentService>();
+
+            // have factory that injects ienumerable<IContentfulClient>, and has client and previewclient properties,
+            //using IContentfulClient.IsPreviewClient
+
+            //todo: missing options
+            //serviceCollection.AddTransient<IContentfulClient>((sp) => {
+            //    var options = sp.GetService<IOptions<ContentfulOptions>>()?.Value;
+            //    options!.UsePreviewApi = true;
+            //    var client = sp.GetService<HttpClient>();
+            //    return new ContentfulClient(client, options);
+        //});
 
             return serviceCollection;
         }
