f412f77bc04d0a5101d883906b8547ab68cbe7fc
Phil Davies <github@phildavies.co.uk>
2022-02-04 13:21:17 +0000

    move IPageService into own file

--- src/SFA.DAS.FindEmploymentSchemes.Web/Services/PageService.cs
+++ src/SFA.DAS.FindEmploymentSchemes.Web/Services/PageService.cs
@@ -6,11 +6,6 @@ using SFA.DAS.FindEmploymentSchemes.Contentful.Model.Content.Interfaces;
 
 namespace SFA.DAS.FindEmploymentSchemes.Web.Services
 {
-    public interface IPageService
-    {
-        (string?, Page?) Page(string pageUrl, IContent content);
-    }
-
     public class PageService : IPageService
     {
         public (string?, Page?) Page(string pageUrl, IContent content)
