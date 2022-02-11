d93862b77c012cef8072e554a0f4346d9246de5b
Ian Rose <ian.rose@digital.education.gov.uk>
2022-02-11 14:47:47 +0000

    Remove case study test data

--- src/SFA.DAS.FindEmploymentSchemes.UnitTests/Web/Models/SchemeTests.cs
+++ src/SFA.DAS.FindEmploymentSchemes.UnitTests/Web/Models/SchemeTests.cs
@@ -38,10 +38,7 @@ namespace SFA.DAS.FindEmploymentSchemes.UnitTests.Web.Models
                     "pay--minimum-wage", "motivations--full-time-role", "motivations--diversity-or-responsibility",
                     "scheme-length--4-months-to-12-months", "scheme-length--a-year-or-more",
                 },
-                new CaseStudy[]
-                {
-                    new CaseStudy("name", "title", new HtmlString(@"<p>Case study content here</p>"))
-                },
+                null,
                 new HtmlString(
                     @"<p>Preamble goes here</p>"),
                 new HtmlString(
