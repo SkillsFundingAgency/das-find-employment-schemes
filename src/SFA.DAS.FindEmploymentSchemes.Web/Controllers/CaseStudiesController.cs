9bac399759a3b51c5fda3eb078c87614ea755bef
Phil Davies <github@phildavies.co.uk>
2022-02-15 11:00:40 +0000

    add unit tests for casestudypage service + tidy up

--- src/SFA.DAS.FindEmploymentSchemes.Web/Controllers/CaseStudiesController.cs
+++ src/SFA.DAS.FindEmploymentSchemes.Web/Controllers/CaseStudiesController.cs
@@ -22,24 +22,6 @@ namespace SFA.DAS.FindEmploymentSchemes.Web.Controllers
             _log = logger;
         }
 
-        //// we _could_ add cache control parameters to the page content type
-        //[HttpGet]
-        //[ResponseCache(Duration = 60*60, Location = ResponseCacheLocation.Any, NoStore = false)]
-        //public IActionResult CaseStudyPage(string pageUrl)
-        //{
-        //    pageUrl = pageUrl.ToLowerInvariant();
-        //    CaseStudyPage page = SchemesContent.CaseStudyPages.FirstOrDefault(p => p.Url.ToLowerInvariant() == pageUrl);
-
-        //    if (pageUrl == "error-check") {
-        //        throw new NotImplementedException("DEADBEEF-DEAD-BEEF-DEAD-BAAAAAAAAAAD");
-        //    }
-
-        //    if (page == null)
-        //        return NotFound();
-
-        //    return View(page);
-        //}
-
         [HttpGet]
         [ResponseCache(Duration = 60 * 60, Location = ResponseCacheLocation.Any, NoStore = false)]
         public IActionResult CaseStudyPage(string pageUrl)
