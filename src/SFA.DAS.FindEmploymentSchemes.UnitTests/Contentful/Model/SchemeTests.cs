59526857434cb6b9d05526b6da8f57321abfce9a
Phil Davies <github@phildavies.co.uk>
2022-02-16 11:22:06 +0000

    unit test wip

---
+++ src/SFA.DAS.FindEmploymentSchemes.UnitTests/Contentful/Model/SchemeTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Html;
+using SFA.DAS.FindEmploymentSchemes.Contentful.Model.Content;
+using Xunit;
+
+namespace SFA.DAS.FindEmploymentSchemes.UnitTests.Contentful.Model
+{
+    public class SchemeTests
+    {
+        [Fact]
+        public void Ctor_ThrowsArgumentExceptionWhenUrlDoesntSanitizeToValidHtmlIdTest()
+        {
+            const string urlThatProducesInvalidId = "---";
+
+            var htmlString = new HtmlString("");
+            Assert.Throws<ArgumentException>(() => new Scheme("", htmlString, htmlString, htmlString, htmlString,
+                urlThatProducesInvalidId, 0, new[] {""} ));
+        }
+    }
+}
