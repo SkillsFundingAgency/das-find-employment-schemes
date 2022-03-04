86fb1f69fcf230e0d858f5fefbd392b8ba1cdebf
Phil Davies <github@phildavies.co.uk>
2022-03-04 12:51:22 +0000

    refactor wip

---
+++ src/SFA.DAS.FindEmploymentSchemes.Contentful/Services/Roots/PageService.cs
@@ -0,0 +1,12 @@
+﻿using SFA.DAS.FindEmploymentSchemes.Contentful.Services.Roots.Base;
+using Contentful.Core.Models;
+
+namespace SFA.DAS.FindEmploymentSchemes.Contentful.Services.Roots
+{
+    public class PageService : ContentRootService
+    {
+        public PageService(HtmlRenderer htmlRenderer) : base(htmlRenderer)
+        {
+        }
+    }
+}
