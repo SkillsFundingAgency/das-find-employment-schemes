e6eaa8b00472c4dc569882ceae62bf310b9af964
Phil Davies <github@phildavies.co.uk>
2022-03-16 12:17:55 +0000

    preview links wip : introduce layoutmodel

---
+++ src/SFA.DAS.FindEmploymentSchemes.Web/Models/LayoutModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.FindEmploymentSchemes.Web.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class LayoutModel
+    {
+        public PreviewModel Preview { get; set; }
+
+        public LayoutModel()
+        {
+            Preview = PreviewModel.NotPreviewModel;
+        }
+    }
+}
