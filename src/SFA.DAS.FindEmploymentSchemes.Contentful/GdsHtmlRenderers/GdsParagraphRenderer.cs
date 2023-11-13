f48d94bf2543215ea986fa1011ce54d85201347a
Shaun Downey <Shaun.Downey@fujitsuuki.onmicrosoft.com>
2023-11-13 16:53:40 +0000

    - Update GDSParagrapgh renderer to handle custom html from contentful.

--- src/SFA.DAS.FindEmploymentSchemes.Contentful/GdsHtmlRenderers/GdsParagraphRenderer.cs
+++ src/SFA.DAS.FindEmploymentSchemes.Contentful/GdsHtmlRenderers/GdsParagraphRenderer.cs
@@ -1,4 +1,6 @@
 ﻿using Contentful.Core.Models;
+using HtmlAgilityPack;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,6 +35,7 @@ namespace SFA.DAS.FindEmploymentSchemes.Contentful.GdsHtmlRenderers
         public bool SupportsContent(IContent content)
         {
             return content is Paragraph;
+
         }
 
         /// <summary>
@@ -43,17 +46,59 @@ namespace SFA.DAS.FindEmploymentSchemes.Contentful.GdsHtmlRenderers
         public async Task<string> RenderAsync(IContent content)
         {
             var paragraph = content as Paragraph;
-            var sb = new StringBuilder();
-            sb.Append("<p class=\"govuk-body\">");
 
-            foreach (var subContent in paragraph!.Content)
+            var sb = new StringBuilder();
+
+            if(paragraph == null)
             {
-                var renderer = _rendererCollection.GetRendererForContent(subContent);
-                sb.Append(await renderer.RenderAsync(subContent));
+
+                return string.Empty;
+
             }
 
-            sb.Append("</p>");
+            // Render any nested content within the paragraph
+            foreach (var subContent in paragraph.Content)
+            {
+
+                // Check if the sub content contains html - process accordingly.
+                if (subContent is Text && ContainsHtml(((Text)subContent).Value))
+                {
+
+                    // If it contains HTML, append it without HTML encoding
+
+                    sb.Append(((Text)subContent).Value);
+
+                }
+                else
+                {
+
+                    sb.Append("<p class=\"govuk-body\">");
+
+                    var renderer = _rendererCollection.GetRendererForContent(subContent);
+
+                    sb.Append(await renderer.RenderAsync(subContent));
+
+                    sb.Append("</p>");
+
+                }
+
+            }
+
             return sb.ToString();
+
         }
+
+        private bool ContainsHtml(string input)
+        {
+
+            var doc = new HtmlDocument();
+
+            doc.LoadHtml(input);
+
+            return doc.DocumentNode.DescendantsAndSelf().Any(n => n.NodeType == HtmlNodeType.Element);
+
+        }
+
     }
+
 }
